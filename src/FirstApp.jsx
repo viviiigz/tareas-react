d7ab8be3c9abf93fca720370b715f03c71b21fb0
Viviana Gonzalez <vivigz06@gmail.com>
2025-10-29 19:52:40 -0300

    primera tarea hecha

---
+++ src/FirstApp.jsx
@@ -0,0 +1,26 @@
+
+//tarea 1: 
+
+// 1. Crear un componente dentro de la carpeta SRC llamado
+//    `FirstApp`
+
+// 2. El FirstApp debe de ser un **Functional Component**
+
+// 3. El contenido del **FirstApp** debe de ser:
+
+//    ```
+//        <h1>Hola desde el FirstApp</h1>
+//    ```
+
+// 4. Agregar en el main.jsx solo el componente de `<FirstApp />`
+
+// ---
+
+
+export const FirstApp = () => {
+  return (
+    <div>
+      <h1>Hola desde FirstApp</h1>
+    </div>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import { FirstApp } from './FirstApp.jsx'
+
+const App = () => {
+  return <FirstApp />;
+};
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
